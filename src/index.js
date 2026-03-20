43b8c723d757e4e174fdf3bc08d87aee97fd318b
Abhinav kumar choubey <abhinavchoubey22@gmail.com>
2026-03-20 07:24:56 +0530

    added chatbot

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const authRouter = require("./Routes/userAuth");
 const { redisC } = require("./config/redis");
 const problemRouter = require("./Routes/problemCreator");
 const submitRouter = require("./Routes/submit");
+const chatRouter=require("./Routes/chatRouter")
 const cors = require("cors");
 
 corsOptions = {
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use("/user", authRouter);
 app.use("/problem", problemRouter);
 app.use("/submission", submitRouter);
+app.use("/chat",chatRouter);
 const run = async () => {
   try {
     await Promise.all([main(), redisC()]);
